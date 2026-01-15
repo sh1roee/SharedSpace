afd4d7e19976389d3a6a6dc56df3195e2201c73d
sh1roee <158986884+sh1roee@users.noreply.github.com>
2026-01-15 14:02:45 +0800

    Revert "chore: fix"
    This reverts commit a2112010b8ff1408b5e250ee441ceee436a7b230.

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.options(/.*/, cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 //connect mongodb
 // This ensures it works whether you use MONGO_URI (code) or MONGODB_URI (README)
